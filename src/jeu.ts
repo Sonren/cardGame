db2e2d5cb5758a6a5bec73ffdc310693bc46902e
Hugo PIARD <E229657C@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-04-06 15:58:25 +0200

    creation d'un programme qui teste les fonctionnalite de jeu.ts

--- src/jeu.ts
+++ src/jeu.ts
@@ -14,12 +14,12 @@ import { ColorCards, Cards, compareCards, createCards, afficheCard } from "./car
 export function createGame () : Array<Cards> {
     let insertCard : Cards = createCards (ColorCards.carreau, 2)
     let cardNumber : number;  
-    let cardGame : Array<Cards>; 
-    for (cardNumber = 3 ; cardNumber < 14 ; cardNumber++ ){
+    let cardGame : Array<Cards> = [insertCard]; 
+    for (cardNumber = 3 ; cardNumber <=14 ; cardNumber++ ){
         insertCard = createCards (ColorCards.carreau, cardNumber);
         cardGame.push(insertCard);
     }
-    for (cardNumber = 2 ; cardNumber < 14 ; cardNumber++){
+    for (cardNumber = 2 ; cardNumber <=14 ; cardNumber++){
         insertCard = createCards (ColorCards.pique, cardNumber);
         cardGame.push(insertCard);
         insertCard = createCards (ColorCards.trèfle, cardNumber);
@@ -39,7 +39,12 @@ export function createGame () : Array<Cards> {
 export function cardsPull (game : Array<Cards> ) : number {
     let tCardPull : Array<Cards>;
     tCardPull = game.splice(0, 1);
-    return tCardPull[1].valueCard;
+    return tCardPull[0].valueCard;
 
 }
 
+
+let tarot : Array<Cards>;
+let cardNumber : number;
+tarot = createGame();
+cardNumber = cardsPull(tarot);
